9dde97d04cf0f358de32949fd4db8e415f5b06bf
emerichunter <areyoudeadyet667@yahoo.fr>
2017-07-20 18:00:22 +0200

    Update micro_insert.sh

--- micro_insert.sh
+++ micro_insert.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+
+TIME_RES=10000 # time resolution in µs 
+
 MONITORING_DB=monitoring_replication
+
+
 PORT_1=5432
 PORT_2=$PORT_1
 PORT_3=$PORT_1
@@ -11,13 +16,11 @@ HOST_3=PCYYYPFE
 
 CONNINFO="postgresql://"$HOST_1":"$","$HOST_2":"$PORT_2","$HOST_3":"$PORT_3"/"$MONITORING_DB"?target_session_attrs=read-write"
 
-#watch -n 2 psql -At -d pgbench -f ideal_sharedbuffers.sql
 while :
 do
 	echo "Pour arreter le script presser [Ctrl +C]"
-	#psql -At -h PCYYYPF6 -U postgres -d monitoring_replication -f insert_monitoring_time.sql ;
 	#/usr/pgsql-10/bin/psql -d "postgresql://PCYYYPF6:5432,PCYYYPFF:5432,PCYYYPFE:5432/monitoring_replication?target_session_attrs=read-write"  -f insert_monitoring_time.sql ;
 	/usr/pgsql-10/bin/psql -d $CONNINFO -f insert_monitoring_time.sql ;
 	#echo "test" ; exit 0 ## TEST
-	usleep 10000 ;
+	usleep $TIME_RES ; 
 done
